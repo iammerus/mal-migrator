852f2ac5e43243a7e07986779315c73b7349dfcb
Melvin Mupondori <melmups@outlook.com>
2020-04-01 19:37:48 +0200

    Added constants for user's usernames, password, etc

---
+++ src/config.py
@@ -0,0 +1,14 @@
+# Your old account's username
+MYA_OLD_ACCOUNT = "GinSwama"
+
+# Your new account's username (where you're migrating to)
+MYA_NEW_ACCOUNT = "iammerus"
+
+# Your new account's password (required to automate adding anime from your old account)
+MY_NEW_ACCOUNT_PASSWORD = "password"
+
+
+#################### Done! Stop editing ####################
+
+
+MYA_BASE_URL = "https://myanimelist.net"
