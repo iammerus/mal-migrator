606633ee80914b2340f159e30f36a22f25339db5
Melvin Mupondori <melmups@outlook.com>
2020-04-01 19:44:33 +0200

    Cleaned up config file. Added PyCharm and VSCode directories to .gitignore

--- src/config.py
+++ src/config.py
@@ -1,14 +1,13 @@
 # Your old account's username
-MYA_OLD_ACCOUNT = "GinSwama"
+MYA_OLD_ACCOUNT: str = "GinSwama"
 
 # Your new account's username (where you're migrating to)
-MYA_NEW_ACCOUNT = "iammerus"
+MYA_NEW_ACCOUNT: str = "iammerus"
 
 # Your new account's password (required to automate adding anime from your old account)
-MY_NEW_ACCOUNT_PASSWORD = "password"
-
+MY_NEW_ACCOUNT_PASSWORD: str = "password"
 
 #################### Done! Stop editing ####################
 
 
-MYA_BASE_URL = "https://myanimelist.net"
+MYA_BASE_URL: str = "https://myanimelist.net"
